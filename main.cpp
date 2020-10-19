18695130768fa9f547485abb32ffd2d0a057e75f
hbaudet <hbaudet@student.42.fr>
2020-10-19 11:25:16 +0200

    scripted backup

--- main.cpp
+++ main.cpp
@@ -1,40 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   list.cpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbaudet <hbaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 10:03:48 by hbaudet           #+#    #+#             */
-/*   Updated: 2020/10/19 10:04:33 by hbaudet          ###   ########.fr       */
+/*   Updated: 2020/10/19 11:01:40 by hbaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "List.hpp"
 
-List::List()
+int main()
 {
-	if (PRINT)
-		std::cout << "Constructor called";
-}
+	ft::List<int> i;
 
-List::~List()
-{
-	if (PRINT)
-		std::cout << "Destructor called";
+	return 0;
 }
-
-List::List(const List& obj)
-{
-	if (PRINT)
-		std::cout << "Copy Constructor called";
-	*this = obj;
-}
-
-List&	List::operator=(const List& obj)
-{
-	if (PRINT)
-		std::cout << "Assignation operator called";
-	return *this;
-}
-
