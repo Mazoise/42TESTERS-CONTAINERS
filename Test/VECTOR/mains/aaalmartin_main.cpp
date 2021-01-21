ca56c1cdc427e96c4f2bd6f2a90d7309edbc8168
hbaudet <hbaudet@student.42.fr>
2021-01-21 13:28:36 +0100

    lmartin_vector ok

--- Test/VECTOR/mains/aaalmartin_main.cpp
+++ Test/VECTOR/mains/aaalmartin_main.cpp
@@ -6,47 +6,16 @@
 /*   By: hbaudet <hbaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 13:07:06 by hbaudet           #+#    #+#             */
-/*   Updated: 2021/01/20 22:03:48 by hbaudet          ###   ########.fr       */
+/*   Updated: 2021/01/21 13:27:34 by hbaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hbaudet_utils.hpp"
 #include "VECTOR_UC.HPP"
 #include <vector>
+#include <iostream>
 #include <iomanip>
-
-/*
-** This is a minimal set of ANSI/VT100 color codes
-*/
-# define _END			"\x1b[0m"
-# define _BOLD			"\x1b[1m"
-# define _UNDER			"\x1b[4m"
-# define _REV			"\x1b[7m"
-
-/*
-** Colors
-*/
-# define _GREY			"\x1b[30m"
-# define _RED			"\x1b[31m"
-# define _GREEN			"\x1b[32m"
-# define _YELLOW		"\x1b[33m"
-# define _BLUE			"\x1b[34m"
-# define _PURPLE		"\x1b[35m"
-# define _CYAN			"\x1b[36m"
-# define _WHITE			"\x1b[37m"
-
-/*
-** Inverted, i.e. colored backgrounds
-*/
-# define _IGREY			"\x1b[40m"
-# define _IRED			"\x1b[41m"
-# define _IGREEN		"\x1b[42m"
-# define _IYELLOW		"\x1b[43m"
-# define _IBLUE			"\x1b[44m"
-# define _IPURPLE		"\x1b[45m"
-# define _ICYAN			"\x1b[46m"
-# define _IWHITE		"\x1b[47m"
-
+#include <limits>
 
 #ifndef STD
 # define NAMESPACE ft
@@ -56,49 +25,32 @@
 
 using namespace NAMESPACE;
 
+
 template <class T>
-void	print(vector<vector<T> >& lst)
+void	print(std::vector<std::vector<T> >& lst)
 {
-	for (typename vector<vector<T> >::iterator it = lst.begin(); it != lst.end(); it++)
+	for (typename std::vector<std::vector<T> >::iterator it = lst.begin(); it != lst.end(); it++)
 	{
-		for (typename vector<T>::iterator it2 = it->begin(); it2 != it->end(); it2++)
+		for (typename std::vector<T>::iterator it2 = it->begin(); it2 != it->end(); it2++)
 			cout << *it2 << ' ';
 		cout << '\n';
 	}
 }
 
 template <class T>
-void	print(vector<T>& lst)
+void	print(std::vector<T>& lst)
 {
-	for (typename vector<T>::iterator it = lst.begin(); it != lst.end(); it++)
+	for (typename std::vector<T>::iterator it = lst.begin(); it != lst.end(); it++)
 		cout << *it << ' ';
 	cout << '\n';
 }
 
 int main()
 {
-	cout << _WHITE << "# test_vector" << _END << std::endl;
-	cout << _YELLOW <<  "/* ********************************************************************** */" << std::endl;
-	cout << "/*                          "<< _WHITE << "BASIC TESTS" << _YELLOW << "                                   */" << std::endl;
-	cout << "/* ********************************************************************** */" << _END << std::endl;
-	cout << std::endl;
+	std::vector<int>	myv;
 
-	vector<int>	myv;
-
-	cout << "vector<" << _PURPLE << "int" << _END << "> myv;" << std::endl;
-
-	cout << std::endl;
 	cout << "myv.max_size(): " << (myv.max_size() > 10000) << std::endl;
-	cout << std::endl;
 
-	try
-	{
-		myv.pop_back();
-	}
-	catch (std::exception &e)
-	{
-		cout << "Catching exception myv: " << e.what() << std::endl;
-	}
 	try
 	{
 		myv.front();
@@ -110,8 +62,6 @@ int main()
 
 	myv.back();
 	
-	cout << _WHITE << "# testing out_of_range exception" << _END << std::endl;
-	cout << "myv.at(0):" << std::endl;
 	try
 	{
 		myv.at(0);
@@ -134,11 +84,9 @@ int main()
 	{
 		cout << "caught : " << e.what() << std::endl;
 	}
-	cout << "myv.reserve(1);" << std::endl;
 	cout << "myv.capacity(): " << myv.capacity() << std::endl;
 	cout << std::endl;
 
-	cout << _WHITE << "# testing reserve exception" << _END << std::endl;
 	cout << "myv.reserve(" << std::numeric_limits<size_t>::max() << ");" << std::endl;
 	try
 	{
@@ -153,25 +101,19 @@ int main()
 	cout << "myv.capacity(): " << myv.capacity() << std::endl;
 
 	myv.push_back(4);
-	cout << _WHITE << "# vector resize more on push_back but both works" << _END << std::endl;
 	cout << "myv.capacity(): " << myv.capacity() << std::endl;
 	myv.erase(myv.begin());
 
-	cout << _WHITE << "# launching clear twice" << _END << std::endl;
+	cout << "# launching clear twice" << std::endl;
 	myv.clear();
 	myv.clear();
 	cout << "myv.capacity(): " << myv.capacity() << std::endl;
 
 
-	cout << _YELLOW <<  "/* ********************************************************************** */" << std::endl;
-	cout << "/*                        "<< _WHITE << "ADVANCED TESTS" << _YELLOW << "                                  */" << std::endl;
-	cout << "/* ********************************************************************** */" << _END << std::endl;
-	cout << std::endl;
 	myv.assign((size_t)10, 8);
 	myv.assign(myv.begin(), myv.begin() + 4); // wtf that works
 
-	vector<int> myv2;
-	cout << "vector<" << _PURPLE << "int" << _END << "> myv2;" << std::endl;
+	std::vector<int> myv2;
 	myv.swap(myv2);
 	myv.swap(myv2);
 	myv.erase(myv.begin() + 2);
@@ -179,7 +121,6 @@ int main()
 	myv.at(2) = 1;
 	myv[1] = 0;
 
-	cout << _WHITE << "# comparaisons tests" << _END << std::endl;
 	cout << std::setw(30) << "myv < myv2: " << (myv < myv2) << std::endl;
 	cout << std::endl;
 	cout << std::setw(30) << "myv > myv2: " << (myv > myv2) << std::endl;
