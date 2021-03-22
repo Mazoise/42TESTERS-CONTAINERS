125d45729b073babe4439ea86166e7d119295d5c
mchardin <mchardin@student.42.fr>
2021-03-22 11:24:57 +0100

    Clean up, removed empty logs

--- .container_tester.sh
+++ .container_tester.sh
@@ -12,12 +12,21 @@ NOPE="\xE2\x9D\x8C"
 WARNING="\xE2\x9A\xA0"
 CONT="$(pwd | rev | cut -f1 -d'/' | rev)"
 
+RESET="\033[0m"
+BOLD="\033[1m"
+BLINK="\033[5m"
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PURPLE="\033[35m"
+
 # FUNCTIONS
 
 run_tests ()
 {
-	rm logs/"$1"_ft.error.log
-	rm logs/"$1"_std.error.log
+	rm logs/"$1"_ft.error.log logs/"$1"_std.error.log
 	clang++ -Werror -Wextra -Werror -std=c++98 -I . -I "$INCLUDES" -I "$INCLUDES"/EMPTY -I ../"$2" -I "$INCLUDES"/EMPTY/JUST_IN_CASE \
 			mains/"$1"_main.cpp -o bin/"$1"_ft 2> logs/"$1"_ft.error.log
 	clang++ -Werror -Wextra -Werror -std=c++98 -D STD -I . -I "$INCLUDES" -I "$INCLUDES"/EMPTY \
@@ -25,14 +34,15 @@ run_tests ()
 
 	if [ $(cat logs/"$1"_std.error.log | wc -c) != "0" ]
 	then
-		echo -e "\033[31m\033[5m$WARNING""\t\033[0m\033[35m\033[1m""$1""_main.cpp seems to be invalid!\033[0m"
+		echo -e "$RED$BLINK$WARNING\t$RESET$PURPLE$BOLD$1_main.cpp seems to be invalid!$RESET"
 	else
 		if [ "$(cat logs/"$1"_ft.error.log | wc -c | rev | cut -f1 -d' ')" != "0" ]
 		then
-			echo -e "\033[1m\033[31m\033[25m""$NOPE""\t""$1" "test does not compile!\033[0m"
+			echo -e "$BOLD$RED\033[25m$NOPE\t$1 test does not compile!$RESET"
 		else
-			./bin/"$1"_std > out/"$1"_std.log 2> /dev/null
-			./bin/"$1"_ft > out/"$1"_ft.log 2> /dev/null
+			rm logs/"$1"_ft.error.log
+			./bin/"$1"_std > out/"$1"_std.log 2> stderror/"$1"_std 
+			./bin/"$1"_ft > out/"$1"_ft.log 2> stderror/"$1"_ft
 			diff out/"$1"_std.log out/"$1"_ft.log > diff/"$1".diff
 			if [ $(cat diff/"$1".diff | wc -l) -eq 0 ]
 			then
@@ -41,21 +51,33 @@ run_tests ()
 					valgrind ./bin/"$1"_ft > /dev/null 2> leak/"$1"_leak
 					if [ "$(grep "All heap blocks were freed -- no leaks are possible" leak/"$1"_leak)" = "" ]
 					then
-						echo -e "\033[31m""$NOPE""\t""$1" "test seems to leak (shame!), or it crashed (shame shame!)"
+						echo -e "$RED$NOPE\t$1 test seems to leak (shame!), or it crashed (shame shame!)$RESET"
 					elif [ "$(grep "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" leak/"$1"_leak)" = "" ]
 					then
-						echo -e "\033[31m""$NOPE""\t""$1" ": Valgrind detected some errors"
+						echo -e "$RED$NOPE\t$1 : Valgrind detected some errors$RESET"
 					else
-						echo -e "$OK""\033[1m\033[32m""\t""$1" 'test passed!'"\033[0m"
+						echo -e "$OK$BOLD$GREEN\t$1 test passed!$RESET"
+						rm leak/"$1"_leak
 					fi
 				else
-					echo -e "$OK""\033[1m\033[32m""\t""$1" 'test passed!'"\033[0m"
+					echo -e "$OK$BOLD$GREEN\t$1 test passed!$RESET"
 				fi
+				rm diff/"$1".diff out/"$1"_std.log out/"$1"_ft.log stderror/"$1"_std stderror/"$1"_ft
 			else
-				echo -e "\033[31m""$NOPE""\t""$1" "test outputs differ. Check logs\033[0m"
+				echo -e "$RED$NOPE\t$1 test outputs differ. Check logs$RESET"
+				if [ $(cat stderror/"$1"_std | wc -c) == "0" ]
+				then
+					rm stderror/"$1"_std
+				fi
+				if [ $(cat stderror/"$1"_ft | wc -c) == "0" ]
+				then
+					rm stderror/"$1"_ft
+				fi
 			fi
 		fi
+		rm logs/"$1"_std.error.log
 	fi
+	rm bin/"$1"_ft bin/"$1"_std
 }
 
 # CHECK OPTIONS
@@ -73,7 +95,7 @@ done
 
 # RUN
 
-rm -rf out bin diff leak logs
+rm -rf out bin diff leak logs stderror
 if [ $RUN = "no" ]
 then
 	exit
@@ -84,14 +106,15 @@ mkdir diff
 mkdir bin
 mkdir leak
 mkdir logs
+mkdir stderror
 
-echo -e "\033[34m\033[1m-" $CONT ":\n\033[0m"
-echo -en "\033[33mFound ${#MAINS[@]} tests for $CONT, "
+echo -e "$BLUE$BOLD-" $CONT ":\n$RESET"
+echo -en "$YELLOW Found ${#MAINS[@]} tests for $CONT, "
 if (( ${#MAINS[@]} > 0 ))
-then
-	echo -e running tests :"\n\033[0m"
+then                 
+	echo -e running tests :"\n$RESET"
 else
-	echo -e jumping to next container."\n\033[0m"
+	echo -e jumping to next container."\n$RESET"
 fi
 
 for M in "${MAINS[@]}"
@@ -99,3 +122,4 @@ do
 	run_tests $M $1 2> /dev/null
 done
 echo
+rm -r bin
